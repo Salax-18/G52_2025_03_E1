99241d62a0b680c82266a0f9206222b93b87dd97
Salax-18 <katheduque08@gmail.com>
2025-08-07 16:25:38 -0500

    Tarea1
    Punto 1 realizado. Se agrego el Script para el punto 2 y el componente2

---
+++ Ejemplo1_CG/Assets/Scripts/Punto2D.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Package2D
+{
+    [Serializable]
+
+    public class Punto2D
+    {
+        private double x;
+        private double y;
+
+
+        public Punto2D()
+        {
+        }
+        public Punto2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public double Distancia(Punto2D otroPunto)
+        {
+            return Math.Sqrt(Math.Pow(otroPunto.X - this.X, 2) + Math.Pow(otroPunto.Y - this.Y, 2));
+        }
+    }
+}
--- Ejemplo1_CG/Assets/Scripts/UsoEstudiante.cs
+++ Ejemplo1_CG/Assets/Scripts/UsoEstudiante.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using PackagePersona; 
+using System.Collections.Generic;  
+using TMPro; // Ensure you have TextMeshPro package installed
 
 public class UsoEstudiante : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI Codigo;
+    public TextMeshProUGUI Carrera;
+    public TextMeshProUGUI Nombre;
+    public TextMeshProUGUI Correo;
+    public TextMeshProUGUI Direccion;
+
+
+    private List<Estudiante> listaE = new List<Estudiante>();
+    private int indiceActual = 0;
+
     void Start()
     {
-        
+        listaE.Add(new Estudiante("2025_03", "Ing Multimedia", "David Castro", "dacastro@uao.edu.co", "Carrera 34"));
+        listaE.Add(new Estudiante("2023_03", "Ing Industrial", "Maria Perez", "merez@uao.edu.co", "Calle 14"));
+        listaE.Add(new Estudiante("2024_01", "Ing Sistemas", "Camilo López", "clopez@uao.edu.co", "Pasoancho"));
+
+        MostrarEstudiante(indiceActual);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarEstudiante(int index)
     {
-        
+        if (index >= 0 && index < listaE.Count)
+        {
+            Estudiante estudiante = listaE[index];
+            Codigo.text = "Nombre: " + estudiante.NameP;
+            Carrera.text = "Correo: " + estudiante.MailP;
+            Nombre.text = "Dirección: " + estudiante.DirP;
+            Correo.text = "Código: " + estudiante.CodeE;
+            Direccion.text = "Carrera: " + estudiante.NameCarreraE;
+        }
+    }
+
+    public void SiguienteEstudiante()
+    {
+        if (indiceActual < listaE.Count - 1)
+        {
+            indiceActual++;
+            MostrarEstudiante(indiceActual);
+        }
+    }
+
+    public void AnteriorEstudiante()
+    {
+        if (indiceActual > 0)
+        {
+            indiceActual--;
+            MostrarEstudiante(indiceActual);
+        }
     }
 }
+
